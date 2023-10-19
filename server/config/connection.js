0d0337b0396e0e1f5482742ecdc03ca6366cab14
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-19 19:59:02 -0400

    add config revisions

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codePal"
-);
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/codePal';
 
-module.exports = mongoose.connection;
+connect(connectionString);
+
+module.exports = connection;
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
+const db = require('./config/connection.js');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,5 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
 });
