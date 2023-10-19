0c6636370c79b3ae213f70f5dfbf3cdccfd9dca7
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-19 19:20:23 -0400

    add pet toys model

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,7 @@
-//* Index file that will export all modules 
-/* 
-TODO:
-*/
-
 const User = require('./user');
 const Pet = require('./pets');
+const Toy = require('./petToys');
+const Food = require('./petFood');
+const Accessory = require('./petAcc');
 
-module.exports = { User, Pet };
+module.exports = { User, Pet, Toy, Food, Accessory };
--- server/models/petToys.js
+++ server/models/petToys.js
@@ -1,10 +1,43 @@
 //* Pet Toys Model 
 /* 
 TODO: 
-- accName (predefined name) 
+- toyName (predefined name) 
 - eH (how much it adds to emotional health)
 - pH (how much it adds to physical health)
-- accAge (age of toy, how long it can be used for)
-- accFor (species the toy can be used for)
+- toyAge (age of toy, how long it can be used for)
+- toyFor (species the toy can be used for)
 */
 
+const { Schema, model } = require('mongoose');
+
+const toySchema = new Schema({
+    toyName: {
+        type: String,
+        enum: ['Ball', 'Mouse', 'String'],  
+        required: true
+    },
+    eH: { 
+        type: Number,
+        required: true,
+        default: 1
+    },
+    pH: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    toyAge: { //* how long toy can be used for (if toyAge == x, toy removed from petToys array )
+        type: Number,
+        required: true,
+        default: 0
+    },
+    toyFor: { //* species toy can be used for
+        type: String,
+        enum: ['cat', 'dog', 'rabbit'],
+        required: true,
+    }
+});
+
+const Toy = model('Toy', toySchema);
+
+module.exports = Toy;
