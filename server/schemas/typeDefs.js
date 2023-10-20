55b2b0019e72150cf1a34a1a01cce21e1b51c7b2
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-20 11:21:39 -0400

    add typeDefs working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,52 +1,67 @@
-const typeDefs = `
-  type Book {
-    _id: ID
-    authors: [String]
-    description: String!
-    bookId: String!
-    image: String
-    link: String
-    title: String!
-  }
+const { gql } = require('apollo-server-express');
+const typeDefs = gql`
+type Accessory {
+    id: ID!
+    accName: String!
+    eH: Int!
+    pH: Int!
+    accAge: Int!
+    accFor: String!
+}
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    savedBooks: [Book]
-  }
+type Food {
+    id: ID!
+    foodName: String!
+    eH: Int!
+    pH: Int!
+    foodAge: Int!
+    foodFor: String!
+}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+type Toy {
+    id: ID!
+    toyName: String!
+    eH: Int!
+    pH: Int!
+    toyAge: Int!
+    toyFor: String!
+}
+
+type Pet {
+    id: ID!
+    species: String!
+    age: Int!
+    petName: String!
+    petDesc: String!
+    pH: Int!
+    eH: Int!
+    petToys: [Toy]
+    PetAcc: [Accessory]
+    PetFood: [Food]
+}
+
+type User {
+    id: ID!
+    email: String!
+    name: String!
+    username: String!
+    password: String!
+    pets: [Pet]
+}
 
   type Query {
-    users: [User]
-    getSingleUser(_id:ID!): User
-    me(token:ID!): User
-  }
-
-  type Mutation {
-    
-    createUser(username: String!, email: String!, password: String!): Auth
-    
-    login(email: String!, password: String!): Auth
-    
-  }
-`;
+  accessories: [Accessory]
+  foods: [Food]
+  toys: [Toy]
+  pets: [Pet]
+  users: [User]
+  accessory(id: ID!): Accessory
+  food(id: ID!): Food
+  toy(id: ID!): Toy
+  pet(id: ID!): Pet
+  user(id: ID!): User
+}`;
 
 module.exports = typeDefs;
 
-// saveBook(
-//   userId:ID!
-//   authors: [String]
-//   description: String!
-//   bookId: String!
-//   image: String
-//   link: String
-//   title: String!
-// ):[User]
 
-// removeBook(_id:ID!, bookId:ID!):[User]
