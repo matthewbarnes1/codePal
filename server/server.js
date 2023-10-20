6179b06748d43bc963ca3a26bc91bed9a8852694
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-20 10:34:07 -0400

    add dependancies

--- server/server.js
+++ server/server.js
@@ -1,22 +1,42 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection.js');
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+
+const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-// app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}!`);
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`kwikMERN Now listening on localhost:${PORT}`);
+      console.log(`GraphQL 🌍 at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
