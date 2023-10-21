871fa505296c165f1ea3a52f8c1ee886664f5548
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-21 13:35:34 -0400

    ADDED config to run client without server

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,9 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      "/graphql": {
-        target: "http://localhost:3001",
-        secure: false,
-        changeOrigin: true,
-      },
-    },
+  },
+  test: {
+    globals: true,
+    environment: "happy dom",
   },
 });
