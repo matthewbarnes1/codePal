991dbda3728058dd158792d7bc78292d0a4e333d
PV <140992905+vasudevap@users.noreply.github.com>
2023-10-22 20:29:39 -0400

    ADDED drag functionality to food bowl - working...

--- client/src/components/ViewComponent.jsx
+++ client/src/components/ViewComponent.jsx
@@ -3,17 +3,23 @@ import React, { useRef, useState, useEffect } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import { AnimatePresence, motion, useDragControls } from "framer-motion";
 
-import HealthStats from "../UI/HealthStats";
 import "./viewComponent.css";
 import MyCodePal from "./CP";
 import foodImg from "../assets/toys/food.png";
+import FluidPanel from "../UI/FluidPanel";
 
 function ViewCodePal() {
-  const [emotional, setEmotional] = useState(5);
+  const [emotional, setEmotional] = useState(8);
   const [physical, setPhysical] = useState(5);
 
   const control = useDragControls();
 
+  const dragArea = {
+    top: 450,
+    bottom: 600,
+    left: 0,
+    right: 1200,
+  };
   const handleDrag = (e) => {
     control.start(e);
   };
@@ -26,25 +32,32 @@ function ViewCodePal() {
       <div className="cp-dashboard">
         <div className="cp-dashboard-title">{"BRUNO"} </div>
 
-        <div className="text-center cp-dashboard-alert">
+        {/* <div className="cp-fluid-panel">
           <HealthStats stats={emotional} />
           <HealthStats stats={physical} />
-        </div>
+        </div> */}
 
         <div className="cp-viewing-area" onPointerDown={handleDrag}>
           <motion.img
             className="cp-viewing-area-left"
-            // whileHover={{ scale: 1.1 }}
-            // whileTap={{ scale: 0.8 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
             // onClick={null}
             whileDrag={{ scale: 2.2 }}
             drag={true}
             dragControls={control}
             dragElastic={1}
-            dragConstraints={{ top: 450, left: 0, right: 500, bottom: 600 }}
+            dragConstraints={{
+              top: dragArea.top,
+              left: dragArea.left,
+              right: dragArea.right,
+              bottom: dragArea.bottom,
+            }}
             dragMomentum={true}
-            dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+            dragTransition={{ bounceStiffness: 60, bounceDamping: 10 }}
             src={foodImg}
+            onDragStart={null} // (event, {info.point.x info.point.y}) trigger codepal image change
+            onDragEnd={null} // (event, info) trigger codepal image change
           />
           <div className="cp-viewing-area-center">
             <div className="cp-viewing-area-codePal">
@@ -54,14 +67,7 @@ function ViewCodePal() {
               {/* </animated.div> */}
             </div>
           </div>
-          <motion.button
-            className="cp-viewing-area-right"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={(e) => handleClick(e)}
-          >
-            {" "}
-          </motion.button>
+          <FluidPanel emotional={emotional} physical={physical} />
         </div>
       </div>
     </main>
