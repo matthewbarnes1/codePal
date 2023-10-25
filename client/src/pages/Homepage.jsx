ad5e6e6de18641355f0ae513add5cfc5252f857b
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-25 10:49:59 -0400

    added homepage and styling

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,29 @@
+import "./homepage.css";
+
 const Homepage = () => {
-  return <div>I am the home page, pls log in</div>;
+  return (
+    <div className="homepage-container">
+      <h1 className="title">Welcome to, CodePal's!</h1>
+      <h2 className="subtitle">The virtual pal for the lonesome coder!
+      <h3>Go to <a href="/signup">Signup</a>to create your account!</h3>
+
+Fill your void with a fun, playful pet without having to scoop the poop!
+</h2>
+
+      <div className="card">
+        <img src="./src/assets/mockup/drink.png" alt="Drink mockup" height="300" width="450" />
+      </div>
+
+      <div className="card">
+        <img src="./src/assets/mockup/hi.png" alt="Hi mockup" height="300" width="450" />
+      </div>
+
+      <div className="card">
+        <img src="./src/assets/mockup/tired.png" alt="Tired mockup" height="300" width="450" />
+      </div>
+
+    </div>
+  );
 };
+
 export default Homepage;
